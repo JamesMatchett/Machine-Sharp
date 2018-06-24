9c806e3b9dd700c940f81987f5f1e1bc4e77c48c
JamesMatchett <30292929+JamesMatchett@users.noreply.github.com>
2018-06-24 22:19:11 +0100

    Include Training.cs will be fleshed out tonight

---
+++ MachineSharpLibrary/MachineSharpLibrary/Training.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineSharpLibrary
+{
+    class Training : NeuralNetwork
+    {
+        //This class inherits neural network and has the ability to correctively alter weightings and biases depending on the set
+        //of expected outputs and actual outputs
+
+            /// <summary>
+            /// Constructs the Training class and through inheritance the neural net to be trained
+            /// </summary>
+            /// <param name="_Inputs"></param>
+            /// <param name="_hidden"></param>
+            /// <param name="_outputs"></param>
+        public Training(int _Inputs, int _hidden, int _outputs) : base(_Inputs, _hidden, _outputs)
+        {
+            
+        }
+
+        
+
+
+    }
+}

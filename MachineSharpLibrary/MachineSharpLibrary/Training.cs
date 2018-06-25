fc6d29e29f9d96b94c6b3940e5364ed07eef75d5
JamesMatchett <30292929+JamesMatchett@users.noreply.github.com>
2018-06-25 10:27:10 +0100

    Begin Training class
    Still a lot to do, namely the nudge functions for either weights, biases or previous activations as well as the recursive call depending on the most "effecient" changes to be made.

--- MachineSharpLibrary/MachineSharpLibrary/Training.cs
+++ MachineSharpLibrary/MachineSharpLibrary/Training.cs
@@ -12,46 +12,96 @@ namespace MachineSharpLibrary
         //of expected outputs and actual outputs
 
         /// <summary>
-        /// Constructs the Training class and through inheritance the neural net to be trained
+        /// Constructs the Training class through the neural net to be trained
         /// </summary>
-        /// <param name="_Inputs"></param>
-        /// <param name="_hidden"></param>
-        /// <param name="_outputs"></param>
-        /// 
-
+        /// <param name="_NeuralNetwork">Neural network to be trained</param>
         private NeuralNetwork neuralNetwork;
         public Training(NeuralNetwork _NeuralNetwork)
         {
             neuralNetwork = _NeuralNetwork;
         }
 
-        //Allow an already existing neural net to be trained
-        
+
+
 
         /// <summary>
         /// Trains the neural net by simulating an input and comparing the expected output with the actual output
         /// </summary>
-        /// <param name="InputArray">The input data to to the neural net</param>
-        /// <param name="ExpectedOutput">The expected output from the neural net</param>
+        /// <param name="InputExpectedOutputList">Pairs of input data and their respective expected outputs</param>
         /// <returns></returns>
-        public Double Train(double[] InputArray, double[] ExpectedOutput)
+        public Double Train(List<InputExpectedOutputPair> InputExpectedOutputList)
         {
-            //Run number through neural net
-            //Compare output with expected output
-            Double[] Result = this.Predict(InputArray);
-            //find best weighting to adjust
+            //Calculate total cost of the network over all examples given
+            double CumulativeCost = 0;
+            foreach (InputExpectedOutputPair IEO in InputExpectedOutputList)
+            {
+                //Run number through neural net
+                //Compare output with expected output
+                CumulativeCost += CalculateCost(IEO);
+            }
+            Double AverageCost = (CumulativeCost / InputExpectedOutputList.Count);
 
-            //find direction to adjust (either up or down)
+            //Now focus on ONE training example
+            //nudge each neuron in approparite direciton
+            //magnitude of nudge is proportional to distance from ideal value
+            Double[] Difference = CalculateDifferencePerNeuron(InputExpectedOutputList.First());
+            
+            //now we can nudge the neuron by either changing bias, changing weighting, change activation from previous layer
+            //Focus on neurons from previous layers with stronger activations as they are more relevant to this single example
+            
+            //TODO Nudge function
 
-            //recursively adjust until local mininum found
-
-            //apply adjustment to neural net
         }
 
-        
+        /// <summary>
+        /// Calculates the cost of one training example for all the whole network
+        /// </summary>
+        /// <param name="IEO"></param>
+        /// <returns></returns>
+        private double CalculateCost(InputExpectedOutputPair IEO)
+        {
+            Double[] Result = neuralNetwork.Predict(IEO.InputArray);
+            //square of the difference between the expected and actual results
+            int iterator = 0;
+            double difference = 0;
+            foreach(double D in IEO.ExpectedOutput)
+            {
+                difference += Math.Pow(D - IEO.ExpectedOutput[iterator], 2);
+                iterator++;
+            }
+            return difference;
+        }
 
-        
 
+
+        /// <summary>
+        /// Returns the difference for each neuron bewteen the expected values and actual values
+        /// </summary>
+        /// <param name="IEO"></param>
+        /// <returns></returns>
+        private double[] CalculateDifferencePerNeuron(InputExpectedOutputPair IEO)
+        {
+            Double[] Result = neuralNetwork.Predict(IEO.InputArray);
+            int iterator = 0;
+            foreach (double D in IEO.ExpectedOutput)
+            {
+                Result[iterator] = D - Result[iterator];
+                iterator++;
+            }
+            return Result;
+        }
+    }
+
+    public class InputExpectedOutputPair
+    {
+        public double[] InputArray;
+        public double[] ExpectedOutput;
+
+        public InputExpectedOutputPair(double[] _InputArray, double[] _ExpectedOutput)
+        {
+            InputArray = _InputArray;
+            ExpectedOutput = _ExpectedOutput;
+        }
 
     }
 }

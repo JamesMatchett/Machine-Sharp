e5f0b19931c87fd1a90db4de4995abd47b3a5f70
JamesMatchett <30292929+JamesMatchett@users.noreply.github.com>
2018-06-25 21:04:30 +0100

    Add 2 classes

---
+++ MachineSharpLibrary/MachineSharpLibrary/Neuron.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineSharpLibrary
+{
+    class Neuron
+    {
+        //first index = weight from neuron to first neuron in next layer
+       public double[] WeightsOut { get; set; }
+       public double Bias { get; set; }
+       public double Activation = 0;
+       
+        /// <summary>
+        /// Create a neuron with empty weights out, bias default to 0
+        /// </summary>
+        /// <param name="NumberOfLayersOut"></param>
+        public Neuron(int NumberOfLayersOut)
+        {
+            WeightsOut = new Double[NumberOfLayersOut - 1];
+            for(int i = 0; i < NumberOfLayersOut; i++)
+            {
+                WeightsOut[i] = 0;
+            }
+            Bias = 0;
+        }
+
+        /// <summary>
+        /// Create a neuron with preset weights out and bias default to 0
+        /// </summary>
+        /// <param name="PresetWeightsOut"></param>
+        public Neuron(Double[] PresetWeightsOut)
+        {
+            WeightsOut = PresetWeightsOut;
+        }
+
+
+        /// <summary>
+        /// Create a neuron with preset weights and bias
+        /// </summary>
+        /// <param name="PresetWeightsOut"></param>
+        /// <param name="_Bias"></param>
+        public Neuron(Double[] PresetWeightsOut, double _Bias)
+        {
+            WeightsOut = PresetWeightsOut;
+            Bias = _Bias;
+        }
+    }
+}
---
+++ MachineSharpLibrary/MachineSharpLibrary/ProvisionalNeuralNetwork.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineSharpLibrary
+{
+    class ProvisionalNeuralNetwork
+    {
+        List<Neuron> InputLayer = new List<Neuron>();
+        List<Neuron> OutputLayer = new List<Neuron>();
+        List<List<Neuron>> HiddenLayers = new List<List<Neuron>>();
+
+        //parameterless constructor to stop the compiler from complaining
+        public ProvisionalNeuralNetwork()
+        { 
+        }
+
+        public ProvisionalNeuralNetwork(int NumberOfHiddenLayers, int[] NeuronsPerHiddenLayer, int NumberOfInputs, int NumberOfOutputs)
+        {
+            //catch for any mismatch errors
+            if(NumberOfHiddenLayers != NeuronsPerHiddenLayer.Count())
+            {
+                //if there are more or less neuron counts for the layers requested then throw an error
+                throw new Exception("Difference between number of hidden layers and neurons supplied for each layer");
+            }
+
+            //fill input layer with neurons
+            for(int i = 0; i < NumberOfInputs; i++)
+            {
+                //if hidden layer exists, number of outputs for each neuron in input layer = number of neurons in first hidden layer
+                if(NeuronsPerHiddenLayer.Count() > 0)
+                {
+                    InputLayer.Add(new Neuron(NeuronsPerHiddenLayer[0]));
+                }
+                else
+                {
+                    //if no hidden layers exist then number of outputs for each neuron in input layer = number of neurons in the output layer
+                    InputLayer.Add(new Neuron(NumberOfOutputs));
+                }
+            }
+
+
+            //fill hidden layer(s) with neurons if any exist
+            if (NumberOfHiddenLayers > 0)
+            {
+                //if only 1 hidden layer then number of outputs from hidden layer = number of neurons in final output layer
+                if(NumberOfHiddenLayers == 1)
+                {
+                    List<Neuron> FirstHiddenLayer = new List<Neuron>();
+                    for(int i = 0; i<NeuronsPerHiddenLayer[0]; i++)
+                    {
+                        FirstHiddenLayer.Add(new Neuron(NumberOfOutputs));
+                    }
+                    HiddenLayers.Add(FirstHiddenLayer);
+                }
+                else
+                {
+                    //more than 1 hidden layer, succesive number of outputs = number of neurons for next layer
+                    int LayerCount = 0;
+                    for(LayerCount = 0; LayerCount < NumberOfHiddenLayers -1 ; LayerCount++)
+                    {
+                        List<Neuron> NextLayer = new List<Neuron>();
+                        for(int i =0; i<NeuronsPerHiddenLayer[LayerCount]; i++)
+                        {
+                            NextLayer.Add(new Neuron(NeuronsPerHiddenLayer[i + 1]));
+                        }
+                        HiddenLayers.Add(NextLayer);
+                    }
+
+                    //for last layer, number of outputs from hidden layer = number of neurons in final output layer
+                    List<Neuron> FinalHiddenLayer = new List<Neuron>();
+                    for (int i = 0; i < NeuronsPerHiddenLayer[NumberOfHiddenLayers-1]; i++)
+                    {
+                        FinalHiddenLayer.Add(new Neuron(NumberOfOutputs));
+                    }
+                    HiddenLayers.Add(FinalHiddenLayer);
+                }
+            }
+
+            //fill output layer with neurons
+            //output layer will always have 1 output per neuron the "result"
+            for(int i =0; i < NumberOfOutputs; i++)
+            {
+                OutputLayer.Add(new Neuron(1));
+            }
+        }
+    }
+}
